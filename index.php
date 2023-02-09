6381994e45f044688952ad872094142ed705e2b1
DontCareCat <zhenya21012001@gmail.com>
2023-02-09 15:28:39 +0200

    fixed index

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 	<form method="POST" action="login.php">
 	<label>USERNAME:</label><input type="text" value="<?php if (isset($_COOKIE["user"])){echo $_COOKIE["user"];}?>" name="username">
 	<label>PASSWORD:</label><input type="password" value="<?php if (isset($_COOKIE)){echo $_COOKIE["password"];}?>" name="password"><br><br>
-	<input type="checkbox" name="rememberme"<?php if (isset($_COOKIE["user"]) && isset($_COOKIE["password"]){echo "checked";})?>> Remember me <br><br>
+	<input type="checkbox" name="rememberme"<?php if (isset($_COOKIE["user"]) && isset($_COOKIE["password"]) {echo "checked";})?>> Remember me <br><br>
 	<input type="submit" value="Login" name="login">
 	</form>
 	<span>
