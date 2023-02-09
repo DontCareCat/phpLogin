ea978644e965a388426b4dfcb71719f89f0ec01e
DontCareCat <zhenya21012001@gmail.com>
2023-02-09 15:45:25 +0200

    locahot is no more

--- dbcon.php
+++ dbcon.php
@@ -1,9 +1,10 @@
 <?php
-	$host='locahost';
+	$host='localhost';
 	$user='ubuntutestwhm_U1';
 	$password='P@ssw0rd';
 	$dbName='ubuntutestwhm_loginDB';
 	$dbCon=mysqli_connect($host,$user,$password,$dbName);
+	echo "done"
 	if(!dbCon){
 		die('Could not connect to mysql server:' .mysql_error());
 	}
